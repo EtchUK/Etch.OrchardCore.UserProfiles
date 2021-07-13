06fd313609854e3d2bc668b48a334ed8e94bb3ce
Joe Berkley <joe.berkley@etchuk.com>
2021-07-13 15:03:13 +0100

    Bump version to v0.3.2-rc2

--- Manifest.cs
+++ Manifest.cs
@@ -6,7 +6,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Users",
     Description = "Adds profiles for users",
     Name = "User Profiles",
-    Version = "0.3.1",
+    Version = "0.3.2",
     Website = "https://etchuk.com"
 )]
 
